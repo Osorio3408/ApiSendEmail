ceb62c4f6b7f1f17b9f9eadaf2e7878eecfd64ca
Osorio3408 <yuliamwow@gmail.com>
2023-02-03 16:21:10 -0500

    subida de archivos

---
+++ SendEmail/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> SendEmail(string email, string subject, string message)
+    {
+        var client = new SmtpClient("smtp.gmail.com", 587)
+        {
+            Credentials = new NetworkCredential("yuliamwow@gmail.com", "nvgbxgokbeiqzjta"),
+            EnableSsl = true
+        };
+        
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("yuliamwow@gmail.com"),
+            To = { email },
+            Subject = subject,
+            Body = message
+        };
+        await client.SendMailAsync(mailMessage);
+        return Ok();
+    }
+}
